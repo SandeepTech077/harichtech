c10038f43ffd9df28f72cbc31b2d70b764c50eb1
SandeepPrajapati07 <sandeep.harichtech@gmail.com>
2025-06-24 03:26:58 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  trailingSlash: true,
-  // output: "export",
   experimental: {
-    serverActions: {}, // ✅ fixed
+    serverActions: {
+      bodySizeLimit: "1mb", // Optional
+      allowedOrigins: ["*"], // Optional
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allow all external images
+      },
+    ],
   },
 };
 
